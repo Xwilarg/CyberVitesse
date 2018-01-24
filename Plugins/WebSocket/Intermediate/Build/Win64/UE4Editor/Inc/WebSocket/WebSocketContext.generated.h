6e987194a2b5fca9bef2fc498b560e1a29e4d192
Xwilarg <xwilarg@yahoo.fr>
2018-01-24 11:43:56 +0100

    Push basic project

---
+++ Plugins/WebSocket/Intermediate/Build/Win64/UE4Editor/Inc/WebSocket/WebSocketContext.generated.h
@@ -0,0 +1,90 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "ObjectMacros.h"
+#include "ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef WEBSOCKET_WebSocketContext_generated_h
+#error "WebSocketContext.generated.h already included, missing '#pragma once' in WebSocketContext.h"
+#endif
+#define WEBSOCKET_WebSocketContext_generated_h
+
+#define OCV_Network_Plugins_WebSocket_Source_WebSocket_Private_WebSocketContext_h_37_RPC_WRAPPERS
+#define OCV_Network_Plugins_WebSocket_Source_WebSocket_Private_WebSocketContext_h_37_RPC_WRAPPERS_NO_PURE_DECLS
+#define OCV_Network_Plugins_WebSocket_Source_WebSocket_Private_WebSocketContext_h_37_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUWebSocketContext(); \
+	friend WEBSOCKET_API class UClass* Z_Construct_UClass_UWebSocketContext(); \
+public: \
+	DECLARE_CLASS(UWebSocketContext, UObject, COMPILED_IN_FLAGS(0), 0, TEXT("/Script/WebSocket"), NO_API) \
+	DECLARE_SERIALIZER(UWebSocketContext) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define OCV_Network_Plugins_WebSocket_Source_WebSocket_Private_WebSocketContext_h_37_INCLASS \
+private: \
+	static void StaticRegisterNativesUWebSocketContext(); \
+	friend WEBSOCKET_API class UClass* Z_Construct_UClass_UWebSocketContext(); \
+public: \
+	DECLARE_CLASS(UWebSocketContext, UObject, COMPILED_IN_FLAGS(0), 0, TEXT("/Script/WebSocket"), NO_API) \
+	DECLARE_SERIALIZER(UWebSocketContext) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define OCV_Network_Plugins_WebSocket_Source_WebSocket_Private_WebSocketContext_h_37_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UWebSocketContext(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UWebSocketContext) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UWebSocketContext); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UWebSocketContext); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UWebSocketContext(UWebSocketContext&&); \
+	NO_API UWebSocketContext(const UWebSocketContext&); \
+public:
+
+
+#define OCV_Network_Plugins_WebSocket_Source_WebSocket_Private_WebSocketContext_h_37_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UWebSocketContext(UWebSocketContext&&); \
+	NO_API UWebSocketContext(const UWebSocketContext&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UWebSocketContext); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UWebSocketContext); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(UWebSocketContext)
+
+
+#define OCV_Network_Plugins_WebSocket_Source_WebSocket_Private_WebSocketContext_h_37_PRIVATE_PROPERTY_OFFSET
+#define OCV_Network_Plugins_WebSocket_Source_WebSocket_Private_WebSocketContext_h_34_PROLOG
+#define OCV_Network_Plugins_WebSocket_Source_WebSocket_Private_WebSocketContext_h_37_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	OCV_Network_Plugins_WebSocket_Source_WebSocket_Private_WebSocketContext_h_37_PRIVATE_PROPERTY_OFFSET \
+	OCV_Network_Plugins_WebSocket_Source_WebSocket_Private_WebSocketContext_h_37_RPC_WRAPPERS \
+	OCV_Network_Plugins_WebSocket_Source_WebSocket_Private_WebSocketContext_h_37_INCLASS \
+	OCV_Network_Plugins_WebSocket_Source_WebSocket_Private_WebSocketContext_h_37_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define OCV_Network_Plugins_WebSocket_Source_WebSocket_Private_WebSocketContext_h_37_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	OCV_Network_Plugins_WebSocket_Source_WebSocket_Private_WebSocketContext_h_37_PRIVATE_PROPERTY_OFFSET \
+	OCV_Network_Plugins_WebSocket_Source_WebSocket_Private_WebSocketContext_h_37_RPC_WRAPPERS_NO_PURE_DECLS \
+	OCV_Network_Plugins_WebSocket_Source_WebSocket_Private_WebSocketContext_h_37_INCLASS_NO_PURE_DECLS \
+	OCV_Network_Plugins_WebSocket_Source_WebSocket_Private_WebSocketContext_h_37_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID OCV_Network_Plugins_WebSocket_Source_WebSocket_Private_WebSocketContext_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ Plugins/WebSocket/Source/WebSocket/WebSocket.Build.cs
@@ -0,0 +1,166 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+using System.IO;
+using UnrealBuildTool;
+
+public class WebSocket : ModuleRules
+{
+    private string ModulePath
+    {
+        get { return ModuleDirectory; }
+    }
+
+    public WebSocket(ReadOnlyTargetRules Target) : base(Target)
+    {
+		
+		PublicIncludePaths.AddRange(
+			new string[] {
+				"WebSocket/Public"
+				// ... add public include paths required here ...
+			}
+			);
+				
+		
+		PrivateIncludePaths.AddRange(
+			new string[] {
+				"WebSocket/Private",
+				// ... add other private include paths required here ...
+			}
+			);
+			
+		
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+                "Json",
+                "JsonUtilities",
+                "Sockets",
+				// ... add other public dependencies that you statically link with here ...
+			}
+			);
+			
+		
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"CoreUObject",
+				"Engine",
+				"Slate",
+				"SlateCore",
+                "Json",
+                "JsonUtilities",
+				// ... add private dependencies that you statically link with here ...	
+			}
+			);
+		
+		
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[]
+			{
+				// ... add any modules that your module loads dynamically here ...
+			}
+			);
+
+
+        if (Target.Platform == UnrealTargetPlatform.Win64)
+        {
+            PrivateDependencyModuleNames.Add("zlib");
+            PrivateDependencyModuleNames.Add("OpenSSL");
+            PrivateIncludePaths.Add("WebSocket/ThirdParty/include/Win64");
+
+            string strStaticPath = Path.GetFullPath(Path.Combine(ModulePath, "ThirdParty/lib/Win64/"));
+            PublicLibraryPaths.Add(strStaticPath);
+
+
+            string[] StaticLibrariesX64 = new string[] {
+                "websockets_static.lib",
+                //"libcrypto.lib",
+                //"libssl.lib",
+            };
+
+            foreach (string Lib in StaticLibrariesX64)
+            {
+                PublicAdditionalLibraries.Add(Lib);
+            }
+        }
+        else if(Target.Platform == UnrealTargetPlatform.Mac)
+        {
+            PrivateIncludePaths.Add("WebSocket/ThirdParty/include/Mac");
+            string strStaticPath = Path.GetFullPath(Path.Combine(ModulePath, "ThirdParty/lib/Mac/"));
+            //PublicLibraryPaths.Add(strStaticPath);
+
+            string[] StaticLibrariesMac = new string[] {
+                "libwebsockets.a",
+                "libssl.a",
+                "libcrypto.a"
+            };
+
+            foreach (string Lib in StaticLibrariesMac)
+            {
+                PublicAdditionalLibraries.Add(Path.Combine(strStaticPath, Lib) );
+            }
+        }
+        else if (Target.Platform == UnrealTargetPlatform.Linux)
+        {
+            PrivateDependencyModuleNames.Add("OpenSSL");
+            PrivateIncludePaths.Add("WebSocket/ThirdParty/include/Linux");
+            string strStaticPath = Path.GetFullPath(Path.Combine(ModulePath, "ThirdParty/lib/Linux/"));
+            PublicLibraryPaths.Add(strStaticPath);
+
+            string[] StaticLibrariesMac = new string[] {
+                "libwebsockets.a",
+                //"libssl.a",
+                //"libcrypto.a"
+            };
+            
+            foreach (string Lib in StaticLibrariesMac)
+            {
+                PublicAdditionalLibraries.Add(Path.Combine(strStaticPath, Lib));
+            }
+        }
+        else if(Target.Platform == UnrealTargetPlatform.IOS)
+        {
+            PrivateIncludePaths.Add("WebSocket/ThirdParty/include/IOS");
+
+            string PluginPath = Utils.MakePathRelativeTo(ModuleDirectory, Target.RelativeEnginePath + "/Source/");
+            PluginPath = PluginPath.Replace("\\", "/");
+
+            string strStaticPath = PluginPath + "/ThirdParty/lib/IOS/";// Path.GetFullPath(Path.Combine(ModulePath, "ThirdParty/lib/IOS/"));
+            PublicLibraryPaths.Add(strStaticPath);
+
+            string[] StaticLibrariesIOS = new string[] {
+                "websockets",
+                "ssl",
+                "crypto"
+            };
+
+            foreach (string Lib in StaticLibrariesIOS)
+            {
+                PublicAdditionalLibraries.Add(Lib);
+                PublicAdditionalShadowFiles.Add(Path.Combine(strStaticPath, "lib" + Lib + ".a") );
+            }
+        }
+        else if(Target.Platform == UnrealTargetPlatform.Android)
+        {
+            PrivateIncludePaths.Add("WebSocket/ThirdParty/include/Android");
+            string strStaticPath = Path.GetFullPath(Path.Combine(ModulePath, "ThirdParty/lib/Android/armeabi-v7a"));
+            PublicLibraryPaths.Add(strStaticPath);
+
+
+            string[] StaticLibrariesAndroid = new string[] {
+                "websockets",
+                //"ssl",
+                //"crypto"
+            };
+
+            foreach (string Lib in StaticLibrariesAndroid)
+            {
+                PublicAdditionalLibraries.Add(Lib);
+            }
+            
+            string PluginPath = Utils.MakePathRelativeTo(ModuleDirectory, Target.RelativeEnginePath);
+            AdditionalPropertiesForReceipt.Add(new ReceiptProperty("AndroidPlugin", Path.Combine(PluginPath, "WebSocket_UPL.xml")));
+        }
+    }
+}
